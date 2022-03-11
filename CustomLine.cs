9f5a84b55e7d0bfbe5b81d2b1b7cfa7863ff346b
Дмитрий Барышев <dmitriy_baryshev@list.ru>
2022-03-12 00:22:37 +0700

    Добавьте файлы проекта.

---
+++ CustomLine.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Windows.ApplicationModel;
+
+namespace DrawingApp.CustomControls
+{
+
+    class CustomLine : Canvas, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private bool _isSelected;
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged(nameof(IsSelected));
+
+                if (IsSelected)
+                {
+                    ellipse1.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                    ellipse2.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                }
+                else
+                {
+                    ellipse1.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                    ellipse2.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                }
+            }
+        }
+
+        public double X1
+        {
+            get { return line.X1; }
+            set
+            {
+                line.X1 = value;
+
+                Canvas.SetLeft(ellipse1, X1 - 5);
+            }
+        }
+
+        public double Y1
+        {
+            get { return line.Y1; }
+            set
+            {
+                line.Y1 = value;
+
+                Canvas.SetTop(ellipse1, Y1 - 5);
+            }
+        }
+
+        public double X2
+        {
+            get { return line.X2; }
+            set
+            {
+                line.X2 = value;
+
+                Canvas.SetLeft(ellipse2, X2 - 5);
+            }
+        }
+
+        public double Y2
+        {
+            get { return line.Y2; }
+            set
+            {
+                line.Y2 = value;
+
+                Canvas.SetTop(ellipse2, Y2 - 5);
+            }
+        }
+
+        private Line line;
+        private Ellipse ellipse1, ellipse2;
+        public CustomLine(ColorPicker colorpicker)
+        {
+
+            line = new Line()
+            {
+                Stroke = new SolidColorBrush(colorpicker.Color),
+                //Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 1,
+            };
+
+            ellipse1 = new Ellipse()
+            {
+                Fill = new SolidColorBrush(Colors.AliceBlue),
+                Stroke = new SolidColorBrush(Colors.Blue),
+                StrokeThickness = 1,
+                Width = 10,
+                Height = 10,
+                Visibility = Windows.UI.Xaml.Visibility.Collapsed,
+            };
+
+            ellipse2 = new Ellipse()
+            {
+                Fill = new SolidColorBrush(Colors.AliceBlue),
+                Stroke = new SolidColorBrush(Colors.Blue),
+                StrokeThickness = 1,
+                Width = 10,
+                Height = 10,
+                Visibility = Windows.UI.Xaml.Visibility.Collapsed,
+            };
+
+            this.Children.Add(line);
+            this.Children.Add(ellipse1);
+            this.Children.Add(ellipse2);
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using DrawingApp.CustomControls;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace DrawingApp
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private List<CustomLine> lines;
+        private CustomLine newLine = null;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            lines = new List<CustomLine>();
+
+            mainCanvas.PointerPressed += OnPointerPressed;
+            mainCanvas.PointerReleased += OnPointerReleased;
+            mainCanvas.PointerMoved += OnPointerMoved;
+
+        }
+
+        private void OnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+
+            var ptrPoint = e.GetCurrentPoint(mainCanvas);
+
+            if (ptrPoint.PointerDevice.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Mouse)
+            {
+                if (ptrPoint.Properties.IsLeftButtonPressed)
+                {
+                    mainCanvas.CapturePointer(e.Pointer);
+
+                    var x = ptrPoint.Position.X;
+                    var y = ptrPoint.Position.Y;
+
+                    newLine = new CustomLine(colorPicker)
+                    {
+                        X1 = x,
+                        Y1 = y,
+                        X2 = x,
+                        Y2 = y,
+                    };
+
+                    lines.Add(newLine);
+                    mainCanvas.Children.Add(newLine);
+                }
+            }
+        }
+        private void OnPointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+
+            var ptrPoint = e.GetCurrentPoint(mainCanvas);
+            if (ptrPoint.PointerDevice.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Mouse)
+            {
+                if (ptrPoint.Properties.IsLeftButtonPressed)
+                {
+                    if (newLine != null)
+                    {
+                        newLine.X2 = ptrPoint.Position.X;
+                        newLine.Y2 = ptrPoint.Position.Y;
+                    }
+                }
+            }
+        }
+        private void OnPointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+
+            var ptrPoint = e.GetCurrentPoint(mainCanvas);
+            if (ptrPoint.PointerDevice.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Mouse)
+            {
+                if (newLine != null)
+                {
+                    if (newLine.X1 == newLine.X2 && newLine.Y1 == newLine.Y2)
+                    {
+                        lines.Remove(newLine);
+                        mainCanvas.Children.Remove(newLine);
+                        newLine = null;
+                        return;
+                    }
+                }
+
+                if (!lines.Any(l => l.IsSelected))
+                {
+                    newLine.IsSelected = true;
+                    newLine = null;
+                }
+            }
+
+            mainCanvas.ReleasePointerCaptures();
+        }
+    }
+}
